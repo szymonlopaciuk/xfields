b4f414c22532d57c910bc5acdd11f3579340a9aa
Szymon Łopaciuk <szymon@lopaciuk.eu>
2022-12-09 11:34:07 +0100

    use MIT faddeeva in OpenMP

--- xfields/fieldmaps/bigaussian_src/faddeeva.h
+++ xfields/fieldmaps/bigaussian_src/faddeeva.h
@@ -6,8 +6,8 @@
 #ifndef XFIELDS_FADDEEVA_H
 #define XFIELDS_FADDEEVA_H
 
-//include_file faddeeva_cernlib.h for_context opencl cuda cpu_openmp
-//include_file faddeeva_mit.h for_context cpu_serial
+//include_file faddeeva_cernlib.h for_context opencl cuda
+//include_file faddeeva_mit.h for_context cpu_serial cpu_openmp
 
 #endif /* XFIELDS_FADDEEVA_H */
 
